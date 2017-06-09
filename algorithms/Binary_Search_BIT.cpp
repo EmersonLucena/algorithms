670369984f07aa94cc6062018c8cc52660ad4ff1
Emerson Leonardo Lucena <emersonleonardolucena@hotmail.com>
2017-06-09 11:32:11 -0300

    Improvements

--- algorithms/Binary_Search_BIT.cpp
+++ algorithms/Binary_Search_BIT.cpp
@@ -1,5 +1,9 @@
 
 
+/*Last index pos where query(pos) <= k
+ *prefix(4) => 1 2 3 4 4 {4} 5 6
+ *prefix(4) => 0 1 1 2 2 {2} 5 6
+ * */
 int prefix(int k, int bit[], int N) {
     int pos = 0, pot = 0;
     while((1 << (pot+1)) <= N) pot++;
@@ -11,5 +15,5 @@ int prefix(int k, int bit[], int N) {
         }
         pot--;
     }
-    return pos;
+    return min(pos, N);
 }
