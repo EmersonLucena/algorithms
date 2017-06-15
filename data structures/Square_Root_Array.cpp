4ab414ad308833144d05db56ba76b31aeed793c4
Emerson Leonardo Lucena <emersonleonardolucena@hotmail.com>
2017-06-15 18:04:52 -0300

    Update Square_Root_Array.cpp

--- data structures/Square_Root_Array.cpp
+++ data structures/Square_Root_Array.cpp
@@ -7,10 +7,6 @@ int sqrtN = 200;
 
 struct bucket {
     vector<T> elem;
-
-    void insert(T e, int idx) {
-        elem.insert(elem.begin() + idx, e);
-    }
 };
 
 vector<bucket> s_array(1);
@@ -22,12 +18,13 @@ void split(bucket &target, int bucket_idx) {
     s_array.insert(s_array.begin() + bucket_idx + 1, temp);
 }
 
-T at(int idx) {
+ ///Returns pair(bucket_index, index_inside_bucket)
+pair<int, int> at(int idx) {
     for(int i=0; i<(int)s_array.size(); i++) {
         bucket &b = s_array[i];
 
         if(idx < b.elem.size())
-            return b.elem[idx];
+            return make_pair(i, idx);
 
         idx -= b.elem.size();
     }
@@ -38,7 +35,7 @@ void insert(T elem, int idx, int bucket_id = 0) {
         bucket &b = s_array[i];
 
         if(idx <= b.elem.size()) {
-            b.insert(elem, idx);
+            b.elem.insert(b.elem.begin() + idx, elem);
             if(b.elem.size() >= 2 * sqrtN) split(b, i);
             return;
         }
@@ -50,7 +47,7 @@ void insert(T elem, int idx, int bucket_id = 0) {
 /**
  Under the premise that the array is sorted
  It is done in 2 * log(sqrt(N)), which is log(N)
- Returns (bucket_index, index_inside_bucket)
+ Returns pair(bucket_index, index_inside_bucket)
 **/
 pair<int, int> lower_bound(T elem) {
     int l = 0, r = s_array.size() - 1, m;
@@ -80,10 +77,26 @@ void ordered_insert(T elem) {
     insert(elem, lbound.second, lbound.first);
 }
 
+void erase(int idx) {
+    pair<int, int> position = at(idx);
+    bucket &b = s_array[position.first];
+
+    b.elem.erase(b.elem.begin() + position.second);
+    if(b.elem.size() == 0) s_array.erase(s_array.begin() + position.first);
+}
+
+void ordered_erase(T elem) {
+    pair<int, int> position = lower_bound(elem);
+    bucket &b = s_array[position.first];
+
+    b.elem.erase(b.elem.begin() + position.second);
+    if(b.elem.size() == 0) s_array.erase(s_array.begin() + position.first);
+}
+
 void print() {
     for(int i=0; i<(int)s_array.size(); i++) {
         bucket &b = s_array[i];
-        if(i) cout
+        if(i) cout << "| ";
 
         for(int j=0; j<(int)b.elem.size(); j++)
             cout << b.elem[j] << " ";
