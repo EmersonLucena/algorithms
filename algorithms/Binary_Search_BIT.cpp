2f9d810e12d9aab4a43972102fddb777a4bbc06f
Emerson Leonardo Lucena <emersonleonardolucena@hotmail.com>
2017-06-21 17:25:15 -0300

    Tested

--- algorithms/Binary_Search_BIT.cpp
+++ algorithms/Binary_Search_BIT.cpp
@@ -1,19 +1,20 @@
 
-
-/*Last index pos where query(pos) <= k
- *prefix(4) => 1 2 3 4 4 {4} 5 6
- *prefix(4) => 0 1 1 2 2 {2} 5 6
+/* Last index pos where query(pos) <= k
+ * prefix(4) => 1 2 3 4 4 {4} 5 6
+ * prefix(4) => 0 1 1 2 2 {2} 5 6
+ * usage: prefix(k, bit, MAXN)
+ * 0 <= pos < N
  * */
 int prefix(int k, int bit[], int N) {
     int pos = 0, pot = 0;
     while((1 << (pot+1)) <= N) pot++;
     while(pot >= 0) {
-        int size = 1 << pot;
-        if(bit[pos + size] <= k) {
+        int size = (1 << pot);
+        if(pos + size < N && bit[pos + size] <= k) {
             k -= bit[pos + size];
             pos += size;
         }
         pot--;
     }
-    return min(pos, N);
+    return pos;
 }
